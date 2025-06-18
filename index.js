5f1fb8aa8ff8af662a06922b0978a7c8679bf5b1
charles <cyphersmae@gmail.com>
2025-06-18 23:33:17 +0300

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+let num1 = 2;
+
+let num2 = 31;
+
+let num3 = 16;
+let num4 = 6;
+console.log(num1+num2+num3+num4)
+
+let multiply = num1 * num2;
+console.log(multiply)
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random)
+
+let mod = num3 % num4
+console.log(mod)
+let max = Math.max(5, 10, 15, 20);
+console.log(max)
+
+
